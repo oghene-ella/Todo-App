bf70f7b7e538708913f6734aec285d0f977b9631
oghene-ella <oghenekaro57@gmail.com>
2023-10-19 18:14:40 +0100

    made some changes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const connectMongo = require("./config/config");
 require("dotenv").config();
 
+const SignUpMiddleware = require("./middleware/signUpMiddleware");
+const LoginMiddleware = require("./middleware/loginMiddleware")
+
 // port
 const PORT = process.env.PORT;
 
@@ -12,10 +15,11 @@ const app = express();
 // use ejs as view engine
 app.set("view engine", "ejs")
 
-// parses incoming requests with JSON payloads
+// parses incoming requests/data to JSON payloads
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
+
 
 // static folder path
 app.use('/public', express.static('public'));
@@ -24,54 +28,18 @@ app.use('/public', express.static('public'));
 app.get("/", (req, res) => {
     res.render("sign_up")
 })
-
 // create signup
-// app.post("/", async (req, res) => {
-//     const data = {
-//         fullname: req.body.fullname,
-//         email: req.body.email,
-//         username: req.body.username,
-//         password: req.body.password
-//     }
+app.post("/", SignUpMiddleware.signUp);
 
-//     const checking = await dbConnection.findOne({ fullname: req.body.fullname });
-
-//     try {
-//         await dbConnection.insertMany([data]);
-//         if (
-//             checking.username === req.body.username &&
-//             checking.password === req.body.password
-//         ) {
-//             res.send("user details already exists");
-//         }
-//     } catch {
-//         res.send("wrong inputs");
-//     }
-
-//     res.status(201).render("home");
-// });
 
 // get method for the login page
 app.get("/login", (req, res) => {
 	res.render("login");
 });
 
-// app.post("/login", async (req, res) => {
-//     try {
-//         const check = await dbConnection.findOne({ username: req.body.username })
+// login as a user
+app.post("/login", LoginMiddleware.login)
 
-//         if (check.password === req.body.password && check.username === req.username) {
-//             res.status(201).render("home")
-//         }
-//         else {
-//             res.send("Wrong username or password")
-//         }
-//     } 
-    
-//     catch (e) {
-//         res.send("Wrong Information")
-//     }
-// })
 
 // get method for the home page
 app.get("/home", (req, res) => {
