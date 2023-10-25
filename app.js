a23471b790d1f334a7ab3f8fb3296503035166f2
oghene-ella <oghenekaro57@gmail.com>
2023-10-25 21:12:17 +0100

    made some minor changes

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require("express");
 const connectMongo = require("./config/config");
 require("dotenv").config();
 
-const SignUpMiddleware = require("./middleware/signUpMiddleware");
-const LoginMiddleware = require("./middleware/loginMiddleware");
-const todoRouter = require("./routes/todoRoute")
+const UsersRouterHandler = require("./users/user.route");
+const TodoRouteHandler = require("./todos/todo.routes")
 
 // port
 const PORT = process.env.PORT;
@@ -15,41 +14,61 @@ const app = express();
 
 // use ejs as view engine
 app.set("view engine", "ejs")
+app.set("views", "views")
 
 // parses incoming requests/data to JSON payloads
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
 
-
 // static folder path
 app.use('/public', express.static('public'));
 
-// get sign up route
-app.get("/", (req, res) => {
-    res.render("sign_up")
-})
-// create signup
-app.post("/", SignUpMiddleware.signUp);
 
+// get index route
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+// get signup page
+app.get("/signup", (req, res) => {
+	res.render("signup");
+});
 
 // get method for the login page
 app.get("/login", (req, res) => {
 	res.render("login");
 });
 
-// login as a user
-app.post("/login", LoginMiddleware.login)
+// // get method for the dashboard page
+// app.get("/dashboard", (req, res) => {
+// 	res.render("dashboard");
+// });
 
 
-// get method for the home page
-app.get("/home", (req, res) => {
-	res.render("index");
-});
+// use users routes
+app.use("/users", UsersRouterHandler);
+app.use("/dashboard", TodoRouteHandler)
 
-app.use("/todo", todoRouter);
+// // error handling
+// app.use((err, req, res, next) => {
+// 	console.error(err.stack);
+// 	res.status(500).json({
+// 		data: null,
+// 		error: "Server Error",
+// 	});
+// });
+
+// get other invalid routes
+// app.get("*", (req, res) => {
+// 	return res.status(404).json({
+// 		data: null,
+// 		error: "Route not found, try again",
+// 	});
+// });
 
 // establish connection to mongodb
+
 connectMongo.connectMongo();
 
 // listener
